629c506c4286c340766f7d7eb1177fbab4b52b82
Osland07 <111648250+Osland07@users.noreply.github.com>
2025-12-04 19:01:59 +0700

    Update index.blade.php

--- resources/views/admin/history/index.blade.php
+++ resources/views/admin/history/index.blade.php
@@ -99,7 +99,7 @@
                                 if (stripos($screening->result_level, 'tinggi') !== false) {
                                     $badgeClass = 'bg-red-100 text-red-700 border border-red-200';
                                 } elseif (stripos($screening->result_level, 'sedang') !== false) {
-                                    $badgeClass = 'bg-orange-100 text-orange-700 border border-orange-200';
+                                    $badgeClass = 'bg-orange-200 text-orange-800 border border-orange-300';
                                 }
                             @endphp
                             <span class="px-3 py-1 rounded-full text-xs font-bold {{ $badgeClass }}">
